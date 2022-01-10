6d4d4bbc816863c790915f696aed7c9e8c7a9384
rohitnegi1028 <rohitnegi1028@gmail.com>
2022-01-10 12:48:30 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import './style.css';
 import DualListBox from './DualListBox/DualListBox';
 
 export default function App() {
+  let options = [];
+  let selected = [];
+
+  let labels = [];
+
+  const onChange = () => {};
+  const deselectColumnOrder = () => {};
+
+  let preserveSelectedOrder = true;
+  let showOrderButtons = true;
+  let canFilter = true;
+
   return (
     <div>
-      <DualListBox />
+      <DualListBox
+        options={options}
+        selected={selected}
+        onChange={onChange}
+        preserveSelectedOrder
+        showOrderButtons
+        deselectColumnOrder={deselectColumnOrder}
+        canFilter
+        labels={labels}
+      />
     </div>
   );
 }
